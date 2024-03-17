ee1944eb8f6a2a82ce69b0edc3c2ab61a6d707a5
koshyar <ensw03420@student.su.edu.krd>
2024-03-17 17:48:14 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addTast(e.target.task.value);
+    form.reset();
+  });
 });
+
+function addTast(value) {
+  let ul = document.querySelector("#list #tasks");
+  let p = document.createElement("p");
+  let li = document.createElement("li");
+  let button = document.createElement("button");
+
+  button.textContent = "X";
+  button.addEventListener("click", (e) => {
+    e.target.parentNode.parentNode.remove();
+  });
+
+  p.textContent = `${value}  `;
+  p.appendChild(button);
+  li.appendChild(p);
+  ul.appendChild(li);
+}
